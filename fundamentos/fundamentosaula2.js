ebc216227e944e55f4f91b1afaccdbea1a92d05e
Isabelasdc <isabelasousa@msn.com>
2024-02-29 22:10:04 -0300

    atualizações aula 2

---
+++ fundamentos/fundamentosaula2.js
@@ -0,0 +1,40 @@
+//comentário de uma linha
+// ajsaosjaosj
+// akosaksapls
+
+//variavel com var
+var nome = "Isabela"
+
+//imprimir informação no console (terminal)
+console.log("IMPRIMINDO UM TEXTO")
+console.log(2)
+console.log(nome)
+console.log(1,2,3,4)
+console.log("NOME:", "Isabela")
+console.log("NOME:", nome)
+console.log("QUALQUER TEXTO" + "QUALQUER OUTRO TEXTO")
+console.log(2 + 2)
+
+//tipos dados
+var texto = "TEXTO"
+console.log("TIPO DA VARIAVEL texto: ", typeof texto)
+
+var numero = 2 
+console.log("TIPO DA VARIAVEL numero: ", typeof numero)
+
+var numeroFlutuante = 2.5444444444
+console.log("TIPO DE VARIAVEL numero: ", typeof numeroFlutuante)
+
+var numeroNegativo = -2.54444444
+console.log("TIPO DE VARIAVEL numero: ", typeof numeroNegativo)
+
+var boleano = true //ou false
+console.log("TIPO DE VARIAVEL boleano: ", typeof boleano)
+
+var array =[1,2,3,4]
+console.log("TIPO DA VARIAVEL array: ", typeof array)
+
+var testeAlteração = "Um texto"
+testeAlteração = 2
+
+console.log("testeAlteracao: ", testeAlteração)
