694b10e34edb52136de650a6724b936c600b045d
Isabelasdc <isabelasousa@msn.com>
2024-03-11 20:23:25 -0300

    lista exercicio att

--- listaExercicios/exercicios/exercicio02.js
+++ listaExercicios/exercicios/exercicio02.js
@@ -1,1 +1,5 @@
 //Escreva um script para ler o número total de eleitores de um município, o número de votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total de eleitores.
+//2. Escreva um script para ler o número total de eleitores de um município, o número de votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total de eleitores.
+
+
+const prompt = require('prompt-sync')();
