76c63748d1abc4803e788e8c7c5186cb61c91207
Isabelasdc <isabelasousa@msn.com>
2024-04-11 20:15:37 -0300

    crud 11-04 middleware

---
+++ 07-crud-produtos-com-valida-middleware/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+// rotas
+const produtoRouter = require('./routes/produtos')
+
+app.use(produtoRouter)
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Aplicação está rodando em http://localhost:3000")
+})
---
+++ 07-crud-produtos-com-valida-middleware/routes/produtos.js
@@ -0,0 +1,116 @@
+const express = require ('express')
+
+const router = express.Router()
+
+
+let listaProdutos = [
+    {   id: 1,
+        nome: "Farinha",
+        preco: 8.99
+    }
+]
+ 
+
+// middleware de validação
+// validar se o produto existe
+
+function validarProduto(req, res, next) {
+    const id =req.params.id
+    const produto = listaProdutos.find(produto => produto.id == id)
+    if(produto){
+        req.produto = produto
+        next()
+    } else {
+        return res.status(404).json({mensagem: "Produto não encontrado!"})
+    }
+    
+    
+}
+ 
+
+
+// READ -> buscar todos os produtos
+router.get('/produtos', (req, res) => {
+    res.status(200).json(listaProdutos)
+    
+})
+
+
+
+// READ -> busca de produtos especificos
+router.get('/produtos/:id' , validarProduto, (req, res) =>{
+    res.json(req.produto)
+})
+
+// validar os atributos do corpo
+
+function validarAtributos(req, res, next) {
+    const dadosRecebidos = req.body
+    if(!dadosRecebidos.nome || !dadosRecebidos.preco){
+        return res.status(400).json({mensagem: "Nome e preço são obrigatórios"})
+    } else {
+        next()
+    }
+    
+}
+
+
+// CREATE -> cadastrar um produto
+
+router.post('/produtos', validarAtributos, (req, res) => {
+    const dados = req.body
+
+        const produto = {
+            id: Math.round(Math.random() * 1000),
+            nome: dados.nome,
+            preco: dados.preco
+        }
+        listaProdutos.push(produto)
+        res.status(201).json(
+            {
+                mensagem: "Produto cadastrado com sucesso!",
+                produto
+            })
+        
+
+})
+
+// UPDATE - alterar produto 
+router.put('/produtos/:id', validarProduto, validarAtributos, (req, res) => {
+    const id = req.params.id
+    const novosDados = req.body
+
+    const index = listaProdutos.findIndex(produto => produto.id == id)
+
+    const produto = {
+        id: Number(id),
+        nome: novosDados.nome,
+        preco: novosDados.preco
+
+    }
+    listaProdutos[index] = produto
+    res.status(200).json(
+    {
+        mensagem: "Produto alterado com sucesso!",
+        produto
+    })
+    
+
+
+})
+
+// DELETE -> excluir produto
+router.delete('/produtos/:id' , validarProduto, (req, res) => {
+    const id = req.params.id
+
+    const index = listaProdutos.findIndex(produto => produto.id == id)
+    
+    listaProdutos.splice(index, 1)
+    res.status(200).json({mensagem: "produto excluido com sucesso!"})
+        
+    
+})
+
+
+
+module.exports = router
