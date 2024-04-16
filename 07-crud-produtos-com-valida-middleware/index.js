5e1d1ea88e731d1406c6694dd0a14b25ee4d42bc
Isabelasdc <isabelasousa@msn.com>
2024-04-15 21:38:23 -0300

    revisão prova

--- 07-crud-produtos-com-valida-middleware/index.js
+++ 07-crud-produtos-com-valida-middleware/index.js
@@ -9,15 +9,6 @@ const produtoRouter = require('./routes/produtos')
 app.use(produtoRouter)
 
 
-
-
-
-
-
-
-
-
-
 app.listen(3000, () => {
     console.log("Aplicação está rodando em http://localhost:3000")
 })
